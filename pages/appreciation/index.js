b12162acd3999433e80f3d58b470999148db784d
TanXin <1148841682@qq.com>
2019-11-25 00:40:37 +0800

    bug fix

--- pages/appreciation/index.js
+++ pages/appreciation/index.js
@@ -28,7 +28,7 @@ Page({
         last = true;
       }
       this.setData({
-        list: res.body.records,
+        list: this.data.list.concat(res.body.records),
         loadAll:last
       })
     })
